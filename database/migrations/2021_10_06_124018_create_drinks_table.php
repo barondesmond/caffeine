ca5a6311f7355eca4c74a82143979840efa6488a
Baron Desmond <baron@desmond.com>
2021-10-06 08:51:48 -0500

    seeds init

---
+++ database/migrations/2021_10_06_124018_create_drinks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDrinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drinks', function (Blueprint $table) {
+            $table->id();
+            $table->string('drink');
+            $table->string('description');
+            $table->integer('caffeine');
+            $table->integer('servings');
+            $table->string('size');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drinks');
+    }
+}
---
+++ database/seeders/DrinksSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+use App\Models\Drinks;
+
+class DrinksSeeder extends Seeder
+{
+    public function run ()
+    {
+        DB::table('drinks')->delete();
+
+        Drinks::create(['drink' => 'Monster Ultra Sunrise',
+          'description' => 'A refreshing orange beverage that has 75mg of caffeine per serving. Every can has two servings.',
+          'caffeine' => 75,
+          'servings' => 2,
+          'size' => '16 oz can'
+        ]);
+        Drinks::create(['drink' => 'Black Coffee',
+          'description' => 'The classic, the average 8oz. serving of black coffee has 95mg of caffeine.',
+          'caffeine' => 95,
+          'servings' => 1,
+          'size' => '8 oz cup'
+        ]);
+        Drinks::create(['drink' => 'Americano',
+          'description' => 'Sometimes you need to water it down a bit... and in comes the americano with an average of 77mg.
+            of caffeine per serving.',
+          'caffeine' => 77,
+          'servings' => 1,
+          'size' => 'drink'
+        ]);
+        Drinks::create(['drink' => 'Sugar free NOS',
+          'description' => 'Another orange delight without the sugar. It has 130 mg. per serving and each can has two servings.',
+          'caffeine' => 130,
+          'servings' => 2,
+          'size' => '16 oz can'
+        ]);
+        Drinks::create(['drink' => '5 Hour Energy',
+          'description' => 'And amazing shot of get up and go! Each 2 fl. oz. container has 200mg of caffeine to get you going.',
+          'caffeine'=> 200,
+          'servings'=> 1,
+          'size' => '2 fl oz'
+        ]);
+    }
+
+}
