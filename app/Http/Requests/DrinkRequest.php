9cf1480c4cbeb7afaf16a7bbc45c46f1522d6408
Baron Desmond <baron@desmond.com>
2021-10-08 08:09:15 -0500

    request

---
+++ app/Http/Requests/DrinkRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use JsonResponse;
+
+class DrinkRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [ 'drinks' => 'int|min:0|max:500'
+            //
+        ];
+    }
+
+    public function response(array $errors)
+    {
+        return new JsonResponse(['error' => $errors], 400);
+    }
+}
